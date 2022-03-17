8c083cd5a0a0e594911493f0cc8dd9e24912cce7
aerastov <a.erastov@gmail.com>
2022-03-18 01:05:22 +0300

    17.03.2022

---
+++ task_1.js
@@ -0,0 +1,16 @@
+const obj = {a: 1, hello: 2, '': 3, eee: 4, var: 5,};
+
+printLogHasOwnObject = (a) => {
+  for (let key in a) {
+    if (a.hasOwnProperty(key)) {
+        console.log(key);
+    };
+};
+};
+
+printLogHasOwnObject(obj);
+
+
+/* Написать, функцию, которая принимает в качестве аргумента объект и выводит в консоль все ключи
+и значения только собственных свойств. Данная функция не должна возвращать значение.
+*/
